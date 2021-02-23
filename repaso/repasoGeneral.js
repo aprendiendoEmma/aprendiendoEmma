1b8c7b8eb2abf1aa6d4d13fe5a62df5e59dcac09
aprendiendoEmma <emma97perez97@gmail.com>
2021-02-23 19:36:34 -0300

    added functions for practice

--- repaso/repasoGeneral.js
+++ repaso/repasoGeneral.js
@@ -9,6 +9,68 @@ const sonFactores = (n, lista ) => {
     return true  
 }
 
-let lista = [1,2,3,4,5];
-let n = 3
-console.log(sonFactores(n, lista));
+
+const maximo = (lista) => {
+    let maximo = lista[0]
+    for (let i = 0; i < lista.length; i++) {
+      if(maximo< lista[i])
+      maximo=lista[i]
+    }
+    return maximo
+}
+
+
+
+const maximoConForEach = (lista) => {
+    let maximo = lista[0]
+    
+    lista.forEach(e =>{
+        if (maximo < e) {
+            maximo = e
+        }
+    })
+    return maximo
+}
+
+
+const maximoIndice = (lista) => {
+    let maximo = lista[0];
+
+    lista.forEach(e => {
+        if (maximo< e) {
+            maximo= e
+            
+        }
+    })
+    return lista.indexOf(maximo);
+}
+
+
+
+const suma = (lista) => { 
+    let suma = 0
+    lista.forEach( e => suma+=e)
+    return suma 
+
+} 
+const promedio = (lista) => {
+    let suma = 0
+    lista.forEach(e => { 
+        suma+=e
+    })
+    return suma / lista.length
+}
+
+
+const tryCatch = (n,n2) =>{
+    if (n < 0 || n2 < 0) {
+        throw new Error('alguno de los numeros ingresados es negativo');
+    } 
+    console.log("me ejecute")
+    return n + n2
+} 
+
+
+let n = -4
+let n2 = 5
+console.log(tryCatch(n,n2))
