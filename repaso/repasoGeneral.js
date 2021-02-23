e5ce2d48b59160302b01a97657b366bfd34fd552
aprendiendoEmma <emma97perez97@gmail.com>
2021-02-23 13:58:37 -0300

    se modifico la carpeta repaso/repasoGeneral.js

--- repaso/repasoGeneral.js
+++ repaso/repasoGeneral.js
@@ -1,6 +1,14 @@
-let n = 6
-while(15 < n){
-    console.log(n);
-    15--
+"use strict";
 
+const sonFactores = (n, lista ) => {
+    for (let i = 0; i < lista.length; i++) {
+        if(n% lista[i] !==0) {
+            return false    
+        }
+    }
+    return true  
 }
+
+let lista = [1,2,3,4,5];
+let n = 3
+console.log(sonFactores(n, lista));
