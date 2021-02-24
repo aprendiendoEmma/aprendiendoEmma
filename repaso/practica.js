86bf033cf0aecc69bb979d20610df905b877ba68
aprendiendoEmma <emma97perez97@gmail.com>
2021-02-24 15:44:37 -0300

    se modidico el archivo repaso/practica.js

--- repaso/practica.js
+++ repaso/practica.js
@@ -3,17 +3,16 @@
 const difference = (list1,list2) => {
     let newList = []
     list1.forEach(e => {
-        list2.forEach(j => {
-            if (e !== j) {
-                newList.push(e)
-            }      
-        });
+        if (!list2.includes(e)) {
+            newList.push(e)
+        }
+
     });
-    return newList;
-
-
+    return newList.join(" ");
 }
 
-let list1 = [1,2,3,4,5]
-let list2 = [3,4,5,6,7]
-console.log(difference(list1,list2));
+
+let list1 = [1,2,3,4,5];
+let list2 = [5,6,7,8,9];
+console.log( difference(list1, list2) )
+
