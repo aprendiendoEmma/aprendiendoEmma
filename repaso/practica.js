f95a03fed4dac2ab7509719480919ccb44b1039e
aprendiendoEmma <emma97perez97@gmail.com>
2021-02-24 16:22:05 -0300

    se modifico el archivo repaso/practicas.js

--- repaso/practica.js
+++ repaso/practica.js
@@ -1,18 +1,11 @@
 "use strict";
 
-const difference = (list1,list2) => {
-    let newList = []
-    list1.forEach(e => {
-        if (!list2.includes(e)) {
-            newList.push(e)
-        }
-
-    });
-    return newList.join(" ");
+let año = parseInt( prompt('ingrese un año' ) );
+if (año% 4==0 || año% 400==0 || año% 100!=0) {
+    console.log('es biciesto');
 }
-
-
-let list1 = [1,2,3,4,5];
-let list2 = [5,6,7,8,9];
-console.log( difference(list1, list2) )
-
+if (año%100!==0) {
+    
+}{
+    console.log('no es biciesto')
+}
