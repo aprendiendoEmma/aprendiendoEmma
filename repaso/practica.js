3075062db337dc0eef8f74f34331ad78f1b41fa0
aprendiendoEmma <emma97perez97@gmail.com>
2021-02-25 14:47:25 -0300

    se modifico el archivo practicas.js

--- repaso/practica.js
+++ repaso/practica.js
@@ -1,9 +1,1 @@
 "use strict";
-const text = (cadena) => {
-    let cadenaInvertida = "" 
-    for (let i = cadena.length ; i >= 0; i--) {
-    cadenaInvertida+= cadena[i]    
-    }
-    return cadenaInvertida
-}
-console.log(text('hola'));
