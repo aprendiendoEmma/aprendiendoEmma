8f07743a05f588100eb18b7808074e51e7f0ade8
aprendiendoEmma <emma97perez97@gmail.com>
2021-03-01 12:14:41 -0300

    se cambio a la carpeta practica

--- repaso/practica.js
+++ repaso/practica.js
@@ -1,1 +1,12 @@
 "use strict";
+
+let numero = parseInt(prompt('ingrese un numero del 1 al 10'));
+    if(numero === 7){
+    console.log('adivinaste');
+    }
+    if (numero> 7) {
+    console.log('te pasaste');
+    }
+    else{
+        console.log('te quedaste corto');
+    }
