b4bacc9de1f4d86af28abcdcb3cdc3b4c3bcec21
aprendiendoEmma <emma97perez97@gmail.com>
2021-03-04 19:51:44 -0300

    se modifico el archivo html

--- objects/Persona.js
+++ objects/Persona.js
@@ -6,14 +6,37 @@ class Persona {
         this.edad = edad
     }
 
-    masJovenQue(otro){
-        if (this.edad < otro.edad) {
-            return true
-        }
-        return false
+    masJovenQue(otro) {
+        return this.edad < otro.edad;
     }
-    y 
+    
+    tocayo(otro) {
+        return this.nombre=== otro.nombre;
+    }
+
+    mismaPersona(otro) {
+         return this.edad === otro.edad && this.nombre === otro.nombre
+    }
+
+    static masJoven(grupo){
+        let masChico = grupo[0]
+        grupo.forEach(persona => {
+            if (persona.edad < masChico.edad) {
+                 masChico = persona
+            }
+        });
+        return masChico
+    }
 }
 
 
 
+const persona1 = new Persona ('marcos', 45);
+const persona2 = new Persona ('emma', 23);
+const persona3 = new Persona ('guille', 25);
+const persona4 = new Persona ('cristian', 3);
+let grupo = [persona1,persona2,persona3,persona4];
+/*console.log(persona1.masJovenQue(persona2));
+console.log(persona1.tocayo(persona2));
+console.log(persona1.mismaPersona2(persona1));*/
+console.log(Persona.masJoven(grupo));
